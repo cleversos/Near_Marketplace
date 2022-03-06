5d691c35cb518376e11f56866a9cfe4821b8f341
CleverSos <xuguangxia725@gmail.com>
2022-03-06 04:51:52 +0000

    link update

--- src/pages/HomePage/components/Footer/Footer.tsx
+++ src/pages/HomePage/components/Footer/Footer.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import DiscordIcon from '../../../../assets/icons/DiscordIcon';
 import InstagramIcon from '../../../../assets/icons/InstagramIcon';
 import TelegramIcon from '../../../../assets/icons/TelegramIcon';
@@ -25,27 +26,63 @@ const Footer = () => {
         <div className="marketplace">
           <BodyText bold>Marketplace</BodyText>
           <ul>
-            <li><BodyText>Collections</BodyText></li>
-            <li><BodyText>All NFTs</BodyText></li>
-            <li><BodyText>Explore</BodyText></li>
+            <li>
+              <Link to={"/collections"}>
+                <BodyText>Collections</BodyText>
+              </Link>
+            </li>
+            <li>
+              <Link to={"/collections"}>
+                <BodyText>All NFTs</BodyText>
+              </Link>
+            </li>
+            <li>
+              <Link to={"/"}>
+                <BodyText>Explore</BodyText>
+              </Link>
+            </li>
           </ul>
         </div>
         <div className="company">
           <BodyText bold>Company</BodyText>
           <ul>
-            <li><BodyText>Privacy Policy</BodyText></li>
+            <li>
+              <a href="https://privacy.galacticway.io" target="_blank">
+                <BodyText>Privacy Policy</BodyText>
+              </a>
+            </li>
             <li><BodyText>Terms of Service</BodyText></li>
             <li><BodyText>Copyright</BodyText></li>
-            <li><BodyText>Careers</BodyText></li>
+            <li>
+              <a href="https://galacticway.freshteam.com/jobs" target="_blank">
+                <BodyText>Careers</BodyText>
+              </a>
+            </li>
           </ul>
         </div>
         <div className="community">
           <BodyText bold>Community</BodyText>
           <ul>
-            <li><BodyText>Twitter</BodyText></li>
-            <li><BodyText>Discord</BodyText></li>
-            <li><BodyText>Help Desk</BodyText></li>
-            <li><BodyText>Blog</BodyText></li>
+            <li>
+              <a href="https://twitter.com/GalacticwayNFT" target="_blank">
+                <BodyText>Twitter</BodyText>
+              </a>
+            </li>   
+            <li>
+              <a href="https://discord.com/invite/yuBjY6QdR6" target="_blank">
+                <BodyText>Discord</BodyText>
+              </a>
+            </li>   
+            <li>
+              <a href="https://help.galacticway.io" target="_blank">
+                <BodyText>Help Desk</BodyText>
+              </a>
+            </li>   
+            <li>
+              <a href="https://galacticway.medium.com" target="_blank">
+                <BodyText>Blog</BodyText>
+              </a>
+            </li>   
           </ul>
         </div>
       </SectionPadding>
