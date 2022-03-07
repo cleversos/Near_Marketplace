541e9a551d288e028daac70b45fba3ddc0aadc0b
sasuke0601 <usakuinaoka61@outlook.com>
2022-03-07 19:04:34 +0900

    collection page gallery sort by done

--- src/pages/CollectionPage/components/GallerySection/GallerySection.tsx
+++ src/pages/CollectionPage/components/GallerySection/GallerySection.tsx
@@ -146,27 +146,6 @@ const GallerySection = (props: GallerySectionProps) => {
 
 export default GallerySection
 
-const blue = {
-  100: '#DAECFF',
-  200: '#99CCF3',
-  400: '#3399FF',
-  500: '#007FFF',
-  600: '#0072E5',
-  900: '#003A75',
-};
-
-const grey = {
-  100: '#E7EBF0',
-  200: '#E0E3E7',
-  300: '#CDD2D7',
-  400: '#B2BAC2',
-  500: '#A0AAB4',
-  600: '#6F7E8C',
-  700: '#3E5060',
-  800: '#2D3843',
-  900: '#1A2027',
-};
-
 const StyledButton = styled('button')(
   ({ theme }) => `
   font-size: 13px;
@@ -238,8 +217,8 @@ const StyledOption = styled(OptionUnstyled)(
   }
 
   &.${optionUnstyledClasses.highlighted} {
-    background-color: ${theme.palette.mode === 'dark' ? grey[800] : grey[100]};
-    color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
+    background-color: #E7EBF0;
+    color: #1A2027;
   }
 
   &.${optionUnstyledClasses.highlighted}.${optionUnstyledClasses.selected} {
